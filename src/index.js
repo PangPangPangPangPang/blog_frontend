d19ee33543ad3f9295c675b77dc4d933b901d55c
wangyefeng <wangyefeng@bytedance.com>
2019-10-23 14:47:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './fonts/Arvo-Regular.ttf'
+import './fonts/Arvo-Bold.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
